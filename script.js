1c5f8d9b8c764e0ab7571a937f90c53a699c50ad
lynx-ship-it <marvellousfakemail@gmail.com>
2026-02-11 08:08:11 +0100

    Add website files

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("JS connected");
+const button = document.getElementById("surpriseBtn");
+const message = document.getElementById("message");
+
+button.addEventListener("click", function () {message.textContent = "You mean alot to me💖"})
+const card = document.getElementById("birthdayCard");
+const bgMusic = document.getElementById("bgMusic");
+const heartsContainer = document.getElementById("heartsContainer");
